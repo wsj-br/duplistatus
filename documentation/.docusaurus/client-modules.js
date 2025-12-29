c95ba117339bb4f92f608632b0797db81db6f2a0
Waldemar Scudeller Jr. <wsj@wsj.com.br>
2025-12-29 01:01:24 -0300

    Remove Docusaurus client files and configuration, including client-manifest.json, client-modules.js, and various documentation JSON files, to streamline the project structure and eliminate unused assets.

--- documentation/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/wsj/src/duplistatus/documentation/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/wsj/src/duplistatus/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/home/wsj/src/duplistatus/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.7_react-dom@19.2.3_react@19.2.3__react@19.2.3_typescript@5.9.3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/wsj/src/duplistatus/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.7_react-dom@19.2.3_react@19.2.3__react@19.2.3_typescript@5.9.3/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/wsj/src/duplistatus/documentation/src/css/custom.css"),
-  require("/home/wsj/src/duplistatus/node_modules/.pnpm/docusaurus-plugin-image-zoom@3.0.1_@docusaurus+theme-classic@3.9.2_@types+react@19.2.7__cae4b44cdb81f647932802ac06a3cd6e/node_modules/docusaurus-plugin-image-zoom/lib/zoom"),
-];
